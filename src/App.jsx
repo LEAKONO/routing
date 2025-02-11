a7c7d02d5effb5e21ef8d0c36e79e011e07c3b00
LEAKONO <leakonoemmanuel3@gmail.com>
2025-02-11 12:17:50 +0300

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Home from '.pages/Home'
 import About from './pages/About'
 import Skills from '.pages/Skills'
@@ -9,9 +10,16 @@ function App() {
 
   return (
     <div className="content">
-      <Home/>
-      <About/>
-      <Skills/>
+      <Router>
+      <Navbar/>
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/services" component={Services} />
+        </Routes>
+      </Router>
+      
     </div>
     
   )
